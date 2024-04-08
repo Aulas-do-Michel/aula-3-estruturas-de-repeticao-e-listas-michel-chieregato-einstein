e81a9b783a8ff1f2a05246e787f98c66b447decf
Michel Chieregato <michel.chieregato@gmail.com>
2024-04-08 16:20:26 -0300

    Update exercicio_2.py

--- exercicio_2.py
+++ exercicio_2.py
@@ -1,39 +1,25 @@
 """
-#### Exercício 2 - Identificar se a variante está no gene BRCA1 - Versão 1.
+#### Exercício 2 - Filtrando uma lista.
 
-Receba 2 inputs do usuário:
-1) O cromossomo de uma variante. Ele virá escrito como texto e da seguinte forma "chr1", "chr2", etc.
-2) A posição dessa variante. Será um número inteiro.
+Receba uma lista de input do usuário.
 
-Responde:
-"Sim" se ela estiver no BRCA1.
-"Não" se ela não estiver.
+Ele digitará como um texto com os números separados por vígula. Para isso, pode-se utilizar o código disponibilizado que
+vai transformar esse texto em lista para você.
 
-Considere que a variante está no gene BRCA1 se estiver no cromossomo 17 (chr17), e se a posição estiver no intevalo de 41196312 a 41277500.
+Depois imprima uma lista apenas com os números ímpares.
 
-Obs: Tirei a localização daqui: https://grch37.ensembl.org/Homo_sapiens/Gene/Summary?g=ENSG00000012048;r=17:41196312-41277500.
+Dica: Crie outra lista e popule ela, a partir da varredura da lista original.
 
 Exemplos:
 
 ----------------------------------
 
-Digite o cromossomo: chrM
-Digite a posição: 41196390
+Digite a sua lista (separando os números por vírgula): 1, 2, 3, 4, 5
 Resposta:
-Não
-
-----------------------------------
-
-Digite o cromossomo: chr17
-Digite a posição: 99
-Resposta:
-Não
-
-----------------------------------
-
-Digite o cromossomo: chr17
-Digite a posição: 41196313
-Resposta:
-Sim
-
+Os números ímpares são [1, 3, 5]
 """
+
+# Código para pegar a lista
+lista = [*map(int, input("Digite a sua lista (separando os números por vírgula): ").split(","))]
+
+# Fazer a partir daqui...
