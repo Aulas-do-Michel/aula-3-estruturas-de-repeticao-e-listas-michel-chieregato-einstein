bc1361eca962bbd809db57bf02974688b2275c33
Michel Chieregato <michel.chieregato@gmail.com>
2024-04-08 16:33:28 -0300

    Update exercicio_3.py

--- exercicio_3.py
+++ exercicio_3.py
@@ -1,57 +1,36 @@
 """
-#### Exercício 3 - Identificar se a variante está no gene BRCA1 - Versão 2.
+#### Exercício 3 - Comparando listas.
 
-Receba 3 inputs do usuário:
-1) O cromossomo de uma variante. Ele virá escrito como texto e da seguinte forma "chr1", "chr2", etc.
-2) A posição dessa variante. Será um número inteiro.
-3) O genoma de referência dessa variante. Considere só 2 opções possíveis, "hg19" e "hg38".
+Receba duas listas de input do usuário. Ele digitará como um texto com os números separados por vígula. 
+Para isso, pode-se utilizar o código disponibilizado que vai transformar esse texto em lista para você.
 
-Responda:
-"Sim" se ela estiver no BRCA1.
-"Não" se ela não estiver.
+Eu quero que você me diga qual das listas tem o maior número dentro delas. 
 
-Considere que a variante está no gene BRCA1 se estiver no cromossomo 17 (chr17), e:
-1) Se a posição estiver no intevalo de 41196312 a 41277500, caso o genoma de referência seja o "hg19".
-2) Se a posição estiver no intevalo de 43044295 a 43125483, caso o genoma de referência seja o "hg38".
-
-Obs: Tirei a Location daqui: https://grch37.ensembl.org/Homo_sapiens/Gene/Summary?g=ENSG00000012048;r=17:41196312-41277500
+Se a primeira lista tiver o maior número, imprima: "Primeira".
+Se a segunda lista tiver o maior número, imprima: "Segunda".
+Se ambas tiverem o mesmo número como maior, digite: "Ambas".
 
 Exemplos:
 
 ----------------------------------
 
-Digite o cromossomo: chr17
-Digite a posição: 41196313
-Digite o genoma de referência: hg38
+Digite a sua primeira lista (separando os números por vírgula): 1, 50, 2, 40
+Digite a sua segunda lista (separando os números por vírgula): 0, 2, 99, 1, 1, 3
 
 Resposta:
-Não
+Segunda
 
 ----------------------------------
 
-Digite o cromossomo: chr17
-Digite a posição: 41196313
-Digite o genoma de referência: hg19
+Digite a sua primeira lista (separando os números por vírgula): 1, 0, 2, 30
+Digite a sua segunda lista (separando os números por vírgula): 9, 9, 9, 30
 
 Resposta:
-Sim
-
-----------------------------------
-
-Digite o cromossomo: chr17
-Digite a posição: 43044299
-Digite o genoma de referência: hg38
-
-Resposta:
-Sim
-
-----------------------------------
-
-Digite o cromossomo: chr2
-Digite a posição: 43044299
-Digite o genoma de referência: hg38
-
-Resposta:
-Não
-
+Ambas
 """
+
+# Código para pegar as listas de input
+primeira_lista = [*map(int, input("Digite a sua primeira lista (separando os números por vírgula): ").split(","))]
+segunda_lista = [*map(int, input("Digite a sua segunda lista (separando os números por vírgula): ").split(","))]
+
+# Fazer a partir daqui
